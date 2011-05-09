7750adef37fd978945b18585811b77dfde58aa9d
Souheil CHELFOUH <trollfot@gmail.com>
2011-05-09 19:34:48 +0200

    Updated tests

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ install_requires = [
 
 
 tests_require = [
+    'cromlech.browser [test]',
     ]
 
 setup(
--- src/dolmen/template/tests/__init__.py
+++ src/dolmen/template/tests/__init__.py
@@ -1,1 +1,4 @@
-# tests package
+import os.path
+
+testdir = os.path.dirname(__file__)
+template_dir = os.path.join(testdir, 'zpt')
